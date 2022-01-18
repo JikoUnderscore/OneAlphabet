e69f784d9d6e6f6933353c34f022672db13895e0
jiko <j1ko@abv.bg>
2022-01-18 12:56:18 +0200

    fixed newline not being inserted

--- js/index.js
+++ js/index.js
@@ -1,52 +1,36 @@
 
 
 function input(){
-    let text_box_input = document.getElementById("input");
+    const text_box_input = document.getElementById("input");
 
-    console.log(text_box_input.innerText.toString());
+    const new_text = translation(text_box_input.value);
 
+    let text_output = document.getElementById("output");
 
-    // const text_input = document.getElementById("input").innerHTML;
+    text_output.value = new_text;
 
-    // console.log(text_input);
-    // let new_text = translation(text_input.toString());
-
-    // const text_output = document.getElementById("output");
-
-    // text_output.innerText = text_input.toString();
-
-}
-
-
-
-
-
-function output(){
-    console.log("output ");
 }
 
 
 function translation(word){
     //  macedonian
     word = word.toLowerCase();
-    word = word.replace("’", "ъ");
-    word = word.replace("њ", "нј");
-    word = word.replace("љ", "лј");
-    word = word.replace("ја", "я");
-    word = word.replace("ја", "я");
-    word = word.replace("ју", "ю");
-    word = word.replace("шт", "щ");
+    word = word.replaceAll("’", "ъ");
+    word = word.replaceAll("њ", "нј");
+    word = word.replaceAll("љ", "лј");
+    word = word.replaceAll("ја", "я");
+    word = word.replaceAll("ја", "я");
+    word = word.replaceAll("ју", "ю");
+    word = word.replaceAll("шт", "щ");
 
 
     //  bulgarian
-    word = word.replace("кь", "ќ");
-    word = word.replace("гь", "ѓ");
-    word = word.replace("й", "ј");
-    word = word.replace("ь", "ј");
-    word = word.replace("дз", "ѕ");
-    word = word.replace("дж", "џ");
-
-
+    word = word.replaceAll("кь", "ќ");
+    word = word.replaceAll("гь", "ѓ");
+    word = word.replaceAll("й", "ј");
+    word = word.replaceAll("ь", "ј");
+    word = word.replaceAll("дз", "ѕ");
+    word = word.replaceAll("дж", "џ");
 
     return word
 }
